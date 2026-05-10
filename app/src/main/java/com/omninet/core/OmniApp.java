96fd041e35ea4ce95cae8ad39e97cdf39ef27c87
bas3543 <93098375+bas3543@users.noreply.github.com>
2026-05-10 05:33:11 +0300

    Update OmniApp.java

--- app/src/main/java/com/omninet/core/OmniApp.java
+++ app/src/main/java/com/omninet/core/OmniApp.java
@@ -2,10 +2,6 @@ package com.omninet.core;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import com.omninet.services.OmniBackgroundService;
-import com.omninet.services.EconomyService;
 
 public class OmniApp extends Application {
 
@@ -13,29 +9,14 @@ public class OmniApp extends Application {
 
     @Override
     protected void attachBaseContext(Context base) {
-        super.attachBaseContext(LocaleManager.setLocale(base));
+        super.attachBaseContext(base);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
-
         OmniID.getOrGenerate(this);
-
-        startOmniServices();
-    }
-
-    private void startOmniServices() {
-        Intent mesh = new Intent(this, OmniBackgroundService.class);
-        Intent econ = new Intent(this, EconomyService.class);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(mesh);
-        } else {
-            startService(mesh);
-        }
-        startService(econ);
     }
 
     public static OmniApp getInstance() {
